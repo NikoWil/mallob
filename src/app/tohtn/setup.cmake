b17890b81082f91abb7bf30941f3f756c2b526ea
Niko Wilhelm <niko.wilhelm@gmx.net>
2022-07-14 14:05:01 +0200

    Add caching of TOHTN sources etc to enable working build

--- src/app/tohtn/setup.cmake
+++ src/app/tohtn/setup.cmake
@@ -6,20 +6,22 @@ set(TOHTN_SOURCES
         src/app/tohtn/tohtn_multi_job.cpp)
 
 # Add Tohtn sources to Mallob base sources
-set(BASE_SOURCES ${BASE_SOURCES} ${TOHTN_SOURCES})
+set(BASE_SOURCES ${BASE_SOURCES} ${TOHTN_SOURCES} CACHE INTERNAL "")
 
 set(BASE_INCLUDES ${BASE_INCLUDES}
         ${PROJECT_SOURCE_DIR}/lib/pandaPIparser/include
-        ${PROJECT_SOURCE_DIR}/src/app/tohtn/crowd)
+        ${PROJECT_SOURCE_DIR}/src/app/tohtn/crowd
+        CACHE INTERNAL "")
 set(BASE_LIBS ${BASE_LIBS}
         ${PROJECT_SOURCE_DIR}/src/app/tohtn/crowd/libcrowd.a
-        ${PROJECT_SOURCE_DIR}/lib/pandaPIparser/libpandaPIparser.a)
+        ${PROJECT_SOURCE_DIR}/lib/pandaPIparser/libpandaPIparser.a
+        CACHE INTERNAL "")
 
-add_library(mallob_tohtn STATIC ${TOHTN_SOURCES})
-set_target_properties(mallob_tohtn PROPERTIES LINKER_LANGUAGE CXX)
-target_include_directories(mallob_tohtn PRIVATE ${BASE_INCLUDES} ${PROJECT_SOURCE_DIR}/lib/pandaPIparser/include
-        ${PROJECT_SOURCE_DIR}/src/app/tohtn/crowd)
-target_compile_options(mallob_tohtn PRIVATE ${BASE_COMPILEFLAGS})
-target_link_libraries(mallob_tohtn PRIVATE ${BASE_LIBS} mallob_commons
-        ${PROJECT_SOURCE_DIR}/src/app/tohtn/crowd/libcrowd.a
-        ${PROJECT_SOURCE_DIR}/lib/pandaPIparser/libpandaPIparser.a)
+#add_library(mallob_tohtn STATIC ${TOHTN_SOURCES})
+#set_target_properties(mallob_tohtn PROPERTIES LINKER_LANGUAGE CXX)
+#target_include_directories(mallob_tohtn PRIVATE ${BASE_INCLUDES} ${PROJECT_SOURCE_DIR}/lib/pandaPIparser/include
+#        ${PROJECT_SOURCE_DIR}/src/app/tohtn/crowd)
+#target_compile_options(mallob_tohtn PRIVATE ${BASE_COMPILEFLAGS})
+#target_link_libraries(mallob_tohtn PRIVATE ${BASE_LIBS} mallob_commons
+#        ${PROJECT_SOURCE_DIR}/src/app/tohtn/crowd/libcrowd.a
+#        ${PROJECT_SOURCE_DIR}/lib/pandaPIparser/libpandaPIparser.a)
